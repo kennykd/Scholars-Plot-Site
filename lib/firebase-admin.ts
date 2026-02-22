5273e7898637604cd28e7273c801fef6c5c86dde
KennyKd <kkrixiadi@gmail.com>
2026-02-22 11:53:03 +0700

    Feat: Addition of Basic Login UI and File Structure

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,14 +1,14 @@
-import admin from "firebase-admin"
+import admin from "firebase-admin";
 
 
 if (!admin.apps.length) {
-    admin.initializeApp({
-        credential: admin.credential.cert({
-            projectId:process.env.PROJECT_ID!,
-            clientEmail:process.env.CLIENT_EMAIL!,
-            privateKey:process.env.PRIVATE_KEY?.replace(/\\n/g, "\n"),
-        }),
-    });
+	admin.initializeApp({
+  	credential: admin.credential.cert({
+			projectId:process.env.FIREBASE_PROJECT_ID!,
+			clientEmail:process.env.FIREBASE_CLIENT_EMAIL!,
+			privateKey:process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+		}),
+	});
 };
 
-export const adminAuth = admin.auth()
+export const adminAuth = admin.auth();
--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,5 +1,5 @@
-import { initializeApp } from "firebase/app";
-
+import { initializeApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -8,8 +8,12 @@ const firebaseConfig = {
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID!,
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET!,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID!,
-  appId: process.env.NEXT_PUBLIC_APP_ID!
+  appId: process.env.NEXT_PUBLIC_APP_ID!,
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig)
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+
+export const auth = getAuth(app);
+
+// const app = initializeApp(firebaseConfig)
