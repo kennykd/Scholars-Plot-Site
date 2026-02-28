5042c2f22d0f6c46256f7fb9e5b2fa34c90c1e06
Barrizzz <barripratama.bp@gmail.com>
2026-02-28 13:47:48 +0700

    feat: prisma integration & swagger documentation testing

--- additional-modules.d.ts
+++ additional-modules.d.ts
@@ -1,4 +1,17 @@
 declare module "*.css" {
-    const content: { [key: String]: string };
+    const content: { [key: string]: string };
     export default content;
+}
+
+declare module "swagger-ui-react" {
+    import * as React from "react";
+
+    interface SwaggerUIProps {
+        url?: string;
+        spec?: Record<string, unknown>;
+        [key: string]: unknown;
+    }
+
+    const SwaggerUI: React.ComponentType<SwaggerUIProps>;
+    export default SwaggerUI;
 }
